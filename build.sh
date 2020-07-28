a1f3a0b965c490640a2bce33686c18f256481b4b
Andrewmatilde <davis6813585853062@outlook.com>
2020-07-28 17:20:40 +0800

    Changes to be committed: 	modified:   build.sh 	new file:   envoy/Dockerfile 	new file:   envoy/build.sh 	new file:   envoy/envoy.yaml 	new file:   example/myapp.yaml 	deleted:    example/sleep.yaml 	modified:   install/helm/sidecar-inject-server/templates/mutatingwebhookconfiguration.yaml 	new file:   pkg/config/example/config copy.yaml 	modified:   pkg/config/example/config.yaml

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o sidecar-inject-server
 docker build --no-cache -t yisaer/sidecar-inject-server:v1 .
 rm -rf sidecar-inject-server
 
-//kind load docker-image yisaer/sidecar-inject-server:v1
+kind load docker-image yisaer/sidecar-inject-server:v1
 
-kubectl delete deployment sleep
+kubectl delete deployment myapp
+
+# helm install sidecar-init install/helm/sidecar-inject-init --namespace sidecar-system 
+
+# helm install sidecar-server install/helm/sidecar-inject-server --namespace sidecar-system --values install/helm/sidecar-inject-server/values.yaml 
---
+++ envoy/build.sh
@@ -0,0 +1,3 @@
+docker build -t andrew/aenvoy:v1 .
+
+//kind load docker-image andrew/aenvoy:v1
